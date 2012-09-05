6407a4e5ff291047551efb864943fd9a9758a8d5
aisensiy <aisensiy@gmail.com>
2012-09-06 01:45:06 +0800

    Init with unit tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5e266f0227b2da396f300740d4d78f45853253f40b1a4e86e6387d8add52018895f357176b59d873008b684cb40c2c9e31436622fd5a6ed1d5b3ada8e898e0a7'

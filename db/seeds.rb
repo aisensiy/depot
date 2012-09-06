24324a51af80e6c5a6977132491549492e4ee3b0
aisensiy <aisensiy@gmail.com>
2012-09-07 02:00:48 +0800

    functional test end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,13 +10,26 @@
 Product.delete_all
 
 Product.create(
-    title: 'Programming Ruby 1.9',
-    description:
-        %{<p>
-            Ruby is the fastest growing and most exciting dynamic language
-            out there. If you need to get working programs delivered fast,
-            you should add Ruby to your toolbox.
-        </p>},
-    image_url: 'http://img1.douban.com/lpic/s11110132.jpg',
-    price: 49.95
+  title: 'Programming Ruby 1.9中',
+  description:
+      %{<p>
+          Ruby is the fastest growing and most exciting dynamic language
+          out there. If you need to get working programs delivered fast,
+          you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'http://img1.douban.com/lpic/s11110132.jpg',
+  price: 49.95
+)
+
+Product.create(
+  title: "结网",
+  description: 
+  %{<p>
+    本书作者一直从事互联网产品的研究和实战，经验丰富，同时作为导师，
+    指导了大量优秀的产品经理，本书的内容也是作者8年来培养产品经理新
+    兵的经验集萃。如果你缺乏培养产品经理的教材，本书正好总结了产品经
+    理知识体系，无疑是你很好的选择。
+  </p>},
+  image_url: 'http://img1.douban.com/mpic/s4254712.jpg',
+  price: 55.00
 )
--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,9 +1,14 @@
+# encoding: UTF-8
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimun: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'AProgramming Ruby 1.9'
+    assert_select '.price', /\$[\d]+\.\d\d/
   end
 
 end

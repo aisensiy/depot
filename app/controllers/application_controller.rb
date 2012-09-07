405450bb246c914682d7b42fb33a6fe88231896c
aisensiy <aisensiy@gmail.com>
2012-09-07 18:34:17 +0800

    Before cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  def current_cart
+    begin
+      Cart.find(session[:cart_id])
+      return cart
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      return cart
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,17 @@ class Product < ActiveRecord::Base
         with: /\.(gif|png|jpg)/i,
         message: 'must be a URL for GIF, PNG or JPG image.'
     }
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items presence')
+        return false
+      end
+    end
 end

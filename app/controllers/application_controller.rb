908d7f4fd9b1dbc09015c37977e86ef919efb9f4
aisensiy <aisensiy@gmail.com>
2012-09-07 18:54:09 +0800

    correct some mistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
+  
   def current_cart
-    begin
-      Cart.find(session[:cart_id])
-      return cart
+    Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      return cart
-    end
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
   end
+
 end

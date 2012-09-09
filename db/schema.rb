a3b9504e535c655843c1255773396fbdb87cc189
aisensiy <aisensiy@gmail.com>
2012-09-09 13:27:47 +0800

    integration finished

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(:version => 20120908115719) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 3
+    t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
   end
--- test/functional/order_notifier_test.rb
+++ test/functional/order_notifier_test.rb
@@ -14,7 +14,7 @@ class OrderNotifierTest < ActionMailer::TestCase
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal ["dave@example.com"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    assert_match /<td>&times;<\/td>\s*<td>Programming Ruby 1.9/, mail.body.encoded
+    assert_match /Programming Ruby 1.9/, mail.body.encoded
   end
 
 end
